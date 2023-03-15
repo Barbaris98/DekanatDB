8519a7e955f8f7851b2344f55e3fbb29d5a9329e
Dmitry Grigorev <d-d12@mail.ru>
2023-03-15 13:40:15 +0400

    заполнил св-ва сущностей

--- Faculty.cs
+++ Faculty.cs
@@ -10,7 +10,7 @@ namespace DekanatDB
     public class Faculty
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        //public List<User> Users { get; set; } = new(); // сотрудники компании
+        public string? NameFaculty { get; set; }
+        public List<Student> Students { get; set; } = new();
     }
 }
--- Student.cs
+++ Student.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace DekanatDB
 {
     public class Student
     {
-        //
+        public int Id { get; set; }
+        public string? LastName { get; set; }
+        public string? Name { get; set; }
+        public string? MiddleName { get; set; }
+        public int Age { get; set; }
+        public bool FullTimeEducation { get; set; }
+
+        public int FacultyId { get; set; }
+        public Faculty? Faculty { get; set; }  // компания пользователя
 
     }
 }
